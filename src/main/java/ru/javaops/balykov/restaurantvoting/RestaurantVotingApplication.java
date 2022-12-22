681caa6f7affda7ddee934bc99e96b93a8e75d6c
Konstantin Balykov <balcon@inbox.ru>
2022-12-22 23:52:32 +0300

    Init

---
+++ src/main/java/ru/javaops/balykov/restaurantvoting/RestaurantVotingApplication.java
@@ -0,0 +1,19 @@
+package ru.javaops.balykov.restaurantvoting;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println("Init");
+    }
+}
